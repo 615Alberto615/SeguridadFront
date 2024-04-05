eae8c9eab9680f8e7180bae65e275a9734c69947
615Alberto615 <a.encinas115@gmail.com>
2024-04-05 00:16:12 -0400

    navbar y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import Home from './components/Home'
 import Informacion from './components/Informacion'
 import Navbar from './components/Navbar'
 import About from './components/About'
-
+import NavbarL from './components/logeado/NavbarL'
 import Newsletter from './components/Newsletter'
 import Fotter from './components/Fotter'
 import { Routes, Route } from 'react-router-dom'; // Importa Routes y Route
 
 function App() {
+  // Variable fija que simula que el usuario está logueado
+  const isLoggedIn = false;
+
   return (
     <>
-      <Navbar/>
+      {isLoggedIn ? <NavbarL/> : <Navbar/>}
       <Routes>
-        <Route path="/" element={<><Home/><Informacion/><About/><Newsletter/><Fotter/></>} />
-        
-        {/* <Route path="/pacientes" element={<Pacientes />} /> */}
+        <Route path="/" element={<><Home /><Informacion /><About /><Newsletter /><Fotter /></>} />
+        {/* Agregar otras rutas aquí según sea necesario */}
       </Routes>
     </>
   );
