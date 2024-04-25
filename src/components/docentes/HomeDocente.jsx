cea1cdee3bbc2ca219a1bf03c6fc339f4eaf8e7a
pmarquez29 <pablo.marquez@ucb.edu.bo>
2024-04-24 20:57:34 -0400

    Banner extra HomeDocente

--- src/components/docentes/HomeDocente.jsx
+++ src/components/docentes/HomeDocente.jsx
@@ -1,8 +1,17 @@
 //import React from 'react';
 //import user from '../assets/user.png';
 
-const HomeDocente = () => {
+import Banner from "../../shared/Banner.jsx";
+import logo from "../../assets/imgbanner_doc.png";
 
+const HomeDocente = () => {
+    return (
+        <div className="md:px-16 p-4 max-w-screen-2xl mx-auto mt-24" id='home'>
+            <Banner banner={logo} heading="Bienvenido Terapeuta!"
+                    subheading="Aqui podras ver tus citas, pacientes y horarios de forma rapida y sencilla."
+            />
+        </div>
+    );
 }
 
 export default HomeDocente;
