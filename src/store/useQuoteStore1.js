f4dcae3768319dd1e0840f33de5ec616edaa995d
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2024-05-17 22:13:33 -0400

    consumo de api para los datos

--- src/store/useQuoteStore1.js
+++ src/store/useQuoteStore1.js
@@ -1,18 +1,30 @@
 // store/useQuoteStore.js
 import { create } from 'zustand';
-import { getQuoteById } from '../service/quoteService1';
+import { getAllQuotes, getQuoteById } from '../service/quoteService1';  
+
 const useQuoteStore = create((set) => ({
     quotes: [],
     selectedQuote: null,
     error: null,
 
+    fetchAllQuotes: async (token) => {
+        try {
+            const response = await getAllQuotes(token);
+            console.log('API Response:', response);
+            set(state => ({ ...state, quotes: response.data, error: null }));
+        } catch (error) {
+            console.error('Error fetching quotes:', error);
+            set(state => ({ ...state, error: error.message }));
+        }
+    },
+
     fetchQuoteById: async (quotesId, token) => {
         try {
             const response = await getQuoteById(quotesId, token);
             console.log('API Response:', response);  // Verifica los datos de la API
-            set(state => ({ ...state, quotes: response.data, selectedQuote: response.data[0], error: null }));
+            set(state => ({ ...state, selectedQuote: response.data, error: null }));
         } catch (error) {
-            console.error('Error fetching quotes:', error);
+            console.error('Error fetching quote:', error);
             set(state => ({ ...state, error: error.message }));
         }
     },
